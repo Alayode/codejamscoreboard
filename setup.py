1f862715f6aa5034e6b493d041d1f16246c45493
Alessio <alessio@wololo.wololo>
2016-04-09 23:22:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+setup(name='Scoreboard Jam',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+      description='Filter CodeJam scoreboard by country',
+      author='Alessio',
+      author_email='defacto133@gmail.com',
+      url='http://scoreboardjam-cimballo.rhcloud.com/',
      install_requires=['Flask>=0.10.1'],
      )
