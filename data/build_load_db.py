a7a5fb8483304f0fa73df6446e101fa7077dc692
syzer3@gmail.com <syzer3@gmail.com>
2016-04-16 07:02:17 +0200

    no tabs (PEP-8), eol on the end of files for better diffs

--- data/build_load_db.py
+++ data/build_load_db.py
@@ -1,5 +1,6 @@
 import sys
 import os
+
 sys.path.append(os.path.realpath('../../'))
 import json
 from scoreboardjam.flaskapp import db, Competitor
@@ -9,14 +10,16 @@ json_name = 'competitorsdata.json'
 db.drop_all()
 db.create_all()
 
-with open(json_name) as data_file:    
+with open(json_name) as data_file:
     data = json.load(data_file)
 
 for row in data:
-	db.session.add(Competitor(username=row['n'],
-		country=row['c'], 
-		country_flag=row['fu'],
-		rank=row['r'],
-		points=row['pts']))
+    db.session.add(
+        Competitor(username=row['n'],
+                   country=row['c'],
+                   country_flag=row['fu'],
+                   rank=row['r'],
+                   points=row['pts']
+                   ))
 
 db.session.commit()
